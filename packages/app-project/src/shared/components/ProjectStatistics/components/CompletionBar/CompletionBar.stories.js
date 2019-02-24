ad36b390ec8d91ccf8e7461436cf47be840ff7bb
Lance <lance@lancesnider.com>
2019-02-23 22:58:57 -0800

    CompletionBar stories

--- packages/app-project/src/shared/components/ProjectStatistics/components/CompletionBar/CompletionBar.stories.js
+++ packages/app-project/src/shared/components/ProjectStatistics/components/CompletionBar/CompletionBar.stories.js
@@ -4,11 +4,25 @@ import zooTheme from '@zooniverse/grommet-theme'
 import { Grommet } from 'grommet'
 import CompletionBar from './CompletionBar'
 
-storiesOf('Shared/Components/CompletionBar', module)
-  .add('CompletionBar', () => (
+storiesOf('Shared/Components/Project Statistics/CompletionBar', module)
+  .add('0%', () => (
+    <Grommet theme={zooTheme}>
+      <CompletionBar
+        completeness={0}
+      />
+    </Grommet>
+  ))
+  .add('50%', () => (
     <Grommet theme={zooTheme}>
       <CompletionBar
         completeness={0.5}
+      />
+    </Grommet>
+  ))
+  .add('99%', () => (
+    <Grommet theme={zooTheme}>
+      <CompletionBar
+        completeness={0.99}
       />
     </Grommet>
   ))
